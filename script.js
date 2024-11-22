b8289d5c3cba6e7f6f5a68cfa94f3582e712e7fe
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-22 19:49:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let colorSelect = document.getElementById("colorSelect");
+let button = document.querySelector('input[type="button"]');
+
+button.addEventListener('click', function() {
+    let selectedOption = colorSelect.options[colorSelect.selectedIndex];
+    
+    colorSelect.removeChild(selectedOption);
+});
